77509f658be35a373f512614e6757e97d54c0824
Ji Hwan <g1767g@naver.com>
2022-04-10 22:05:04 +0900

    almost

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,5 +1,3 @@
-
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,6 +6,7 @@ import java.util.PriorityQueue;
 class Node implements Comparable<Node> {
     public int key;
     public String c;
+    public String pre = "";
     Node left;
     Node right;
 
@@ -24,7 +23,8 @@ class Node implements Comparable<Node> {
 
     @Override
     public int compareTo(Node o) {
-        return 0;
+
+        return key-o.key;
     }
 }
 
@@ -88,21 +88,53 @@ public class Huffman {
 
 
 
-
-
     static void print(Node node){
         if(node != null) {
             if(node.left != null) print(node.left);
             if(node.left == null && node.right == null)
-                System.out.println(node.key + " "+ node.c);
+                System.out.println(node.c + " : "+ node.key+", "+node.pre);
             if(node.right != null) print(node.right);
         }
     }
 
-    static void HuffmanNode(PriorityQueue<Node> priorityQueue){
+    static void getPrefix(Node node,String prefix){
+        if(node != null) {
+            prefix = prefix + node.pre;
+            if(node.left != null) getPrefix(node.left,prefix);
+            if(node.right != null) getPrefix(node.right,prefix);
+            if(node.left == null && node.right == null){
+                System.out.println(node.key+" "+node.c+",pre : "+prefix);
+            }
+
+        }
+
+    }
+
+    static void setPrefix(Node node){
+        if(node== null) {
+            return;
+        }
+
+
+        if(node.left != null)
+            node.left.pre = node.left.pre + "0";
+        if(node.right!=null)
+            node.right.pre = node.right.pre + "1";
+
+        if(node.left != null) {
+            setPrefix(node.left);
+        }
+        if(node.right != null) {
+            setPrefix(node.right);
+        }
+
+    }
+
+    static void huffmanNode(PriorityQueue<Node> priorityQueue){
         while (true) {
             if(priorityQueue.size()==1)
                 return;
+
             Node node = new Node();
             node.left = priorityQueue.poll();
             node.right = priorityQueue.poll();
@@ -116,7 +148,18 @@ public class Huffman {
 
     public static void main(String[] args) {
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
-//        priorityQueue.add(new Node(7,'a'));
+
+        priorityQueue.add(new Node(90,"T"));
+        priorityQueue.add(new Node(120,"G"));
+        priorityQueue.add(new Node(270,"C"));
+        priorityQueue.add(new Node(450,"A"));
+
+
+//        priorityQueue.add(new Node(12,"t"));
+//        priorityQueue.add(new Node(15,"e"));
+//        priorityQueue.add(new Node(4,"s"));
+//        priorityQueue.add(new Node(6,"i"));
+//        priorityQueue.add(new Node(8,"n"));
 
 
 
@@ -124,34 +167,39 @@ public class Huffman {
 
 
 
-
-        File file = new File();
-        String text = file.getString("test.txt");
-        System.out.println(text);
-
-        Check check = new Check();
-        String[] strArr = new String[100];
-        int[] countArr = new int[100];
-        check.CheckCount(strArr,countArr,text);
-        check.print(strArr,countArr);
-
+//        File file = new File();
+//        String text = file.getString("test.txt");
+//        System.out.println(text);
+//
+//        Check check = new Check();
+//        String[] strArr = new String[100];
+//        int[] countArr = new int[100];
+//        check.CheckCount(strArr,countArr,text);
+//        check.print(strArr,countArr);
 
 
 
-        for(int i=0;i<strArr.length;i++){
-            if(strArr[i]!=null){
-                Node node = new Node();
-                node.key = countArr[i];
-                node.c = strArr[i];
-                node.left = node.right = null;
-                priorityQueue.add(node);
-            }
-        }
+//        for(int i=0;i<strArr.length;i++){
+//            if(strArr[i]!=null){
+//                Node node = new Node();
+//                node.key = countArr[i];
+//                node.c = strArr[i];
+//                node.left = node.right = null;
+//                priorityQueue.add(node);
+//            }
+//        }
 
         Node node = new Node();
-        HuffmanNode(priorityQueue);
+        huffmanNode(priorityQueue);
         node = priorityQueue.poll();
+        setPrefix(node);
         print(node);
+        System.out.println();
+        String prefix = "";
+        getPrefix(node,prefix);
+
+
+
 
 
 
