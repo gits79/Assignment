7f2954202f70ed5b3fca1aa3304d7b8e8cbd4b0c
Ji Hwan <g1767g@naver.com>
2022-04-12 01:04:23 +0900

    finish

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 class Node implements Comparable<Node> {
@@ -28,6 +29,12 @@ class Node implements Comparable<Node> {
     }
 }
 
+class Prefix{
+    String c;
+    String prefix;
+    int key;
+}
+
 class File{
     public String getString(String title){
         String str = "";
@@ -36,7 +43,6 @@ class File{
             while((i= fis.read())!=-1){
                 str = str + (char)i;
             }
-
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -86,24 +92,18 @@ class Check{
 
 public class Huffman {
 
-
-
-    static void print(Node node){
-        if(node != null) {
-            if(node.left != null) print(node.left);
-            if(node.left == null && node.right == null)
-                System.out.println(node.c + " : "+ node.key+", "+node.pre);
-            if(node.right != null) print(node.right);
-        }
-    }
-
-    static void getPrefix(Node node,String prefix){
+    static void getPrefix(Node node,String prefix,ArrayList<Prefix> arrayList){
         if(node != null) {
             prefix = prefix + node.pre;
-            if(node.left != null) getPrefix(node.left,prefix);
-            if(node.right != null) getPrefix(node.right,prefix);
+            if(node.left != null) getPrefix(node.left,prefix,arrayList);
+            if(node.right != null) getPrefix(node.right,prefix,arrayList);
             if(node.left == null && node.right == null){
-                System.out.println(node.key+" "+node.c+",pre : "+prefix);
+                Prefix preClass = new Prefix();
+                preClass.prefix = prefix;
+                preClass.c = node.c;
+                preClass.key = node.key;
+                arrayList.add(preClass);
+                System.out.println("문자: "+node.c +",  빈도수: "+node.key+", 프리픽스 : "+prefix);
             }
 
         }
@@ -114,7 +114,6 @@ public class Huffman {
         if(node== null) {
             return;
         }
-
 
         if(node.left != null)
             node.left.pre = node.left.pre + "0";
@@ -148,67 +147,112 @@ public class Huffman {
 
     public static void main(String[] args) {
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
-
-        priorityQueue.add(new Node(90,"T"));
-        priorityQueue.add(new Node(120,"G"));
-        priorityQueue.add(new Node(270,"C"));
-        priorityQueue.add(new Node(450,"A"));
+        ArrayList<Prefix> arrayList = new ArrayList<>();
 
 
-//        priorityQueue.add(new Node(12,"t"));
-//        priorityQueue.add(new Node(15,"e"));
-//        priorityQueue.add(new Node(4,"s"));
-//        priorityQueue.add(new Node(6,"i"));
-//        priorityQueue.add(new Node(8,"n"));
+        // 파일 읽어오기
+        File file = new File();
+        String text = file.getString("test.txt");
+        String originalData = text;
 
 
 
+        // 빈도수 계산
+        Check check = new Check();
+        String[] strArr = new String[2000];
+        int[] countArr = new int[2000];
+        check.CheckCount(strArr,countArr,text);
+        System.out.printf("각 문자별 빈도수: ");
+        check.print(strArr,countArr);
+        System.out.println();
 
 
 
 
-//        File file = new File();
-//        String text = file.getString("test.txt");
-//        System.out.println(text);
-//
-//        Check check = new Check();
-//        String[] strArr = new String[100];
-//        int[] countArr = new int[100];
-//        check.CheckCount(strArr,countArr,text);
-//        check.print(strArr,countArr);
+        // 우선순위 큐에 넣기
+        for(int i=0;i<strArr.length;i++){
+            if(strArr[i]!=null){
+                Node node = new Node();
+                node.key = countArr[i];
+                node.c = strArr[i];
+                node.left = node.right = null;
+                priorityQueue.add(node);
+            }
+        }
 
-
-
-//        for(int i=0;i<strArr.length;i++){
-//            if(strArr[i]!=null){
-//                Node node = new Node();
-//                node.key = countArr[i];
-//                node.c = strArr[i];
-//                node.left = node.right = null;
-//                priorityQueue.add(node);
-//            }
-//        }
-
+        //허프만 트리 만들기 및 프리픽스 부여
         Node node = new Node();
         huffmanNode(priorityQueue);
         node = priorityQueue.poll();
         setPrefix(node);
-        print(node);
         System.out.println();
         String prefix = "";
-        getPrefix(node,prefix);
+        getPrefix(node,prefix,arrayList);
+
+
+        // 프리픽스 가져오기 및 인코딩
+        String encoding = text;
+        for(int j=0;j<encoding.length();j++) {
+            String temp = encoding.substring(j,j+1);
+            for (int i = 0; i < arrayList.size(); i++) {
+                Prefix pre = new Prefix();
+                pre = arrayList.get(i);
+                if(temp.equals(pre.c))
+                    encoding = encoding.replaceFirst(temp,pre.prefix);
+            }
+        }
 
 
 
+        // 디코딩
+        String decoding = encoding;
+        for(int j=0;j<decoding.length();j++) {
+            String temp;
+            for (int i = 0; i < arrayList.size(); i++) {
+
+                Prefix pre = new Prefix();
+                pre = arrayList.get(i);
+                String decode = pre.prefix;
+                int len = decode.length();
 
 
+                // 맨 마지막에 프리픽스가 남은 인코딩 숫자보다 큰경우 제거
+                if(j+len>decoding.length())
+                    temp = decoding.substring(j,decoding.length());
+                else
+                    temp = decoding.substring(j,j+len );
 
 
+                if(decode.equals(temp))
+                    decoding = decoding.replaceFirst(temp,pre.c);
+//                System.out.println("j: "+j+",    decode:  "+decode+",   temp: "+temp+"   , decoding: "+decoding+"  text.len: "+decoding.length());
+                if(!decoding.contains("0") && !decoding.contains("1"))
+                    break;
+
+            }
+        }
 
 
+        System.out.println();
+        System.out.println("원래 데이터: "+text);
+        System.out.println("인코딩 후 데이터: "+encoding);
+        System.out.println("디코딩 후 데이터: "+decoding);
+        System.out.println();
 
 
+        // 압축전 파일의 크기
+        int textSize = text.length()*8;
+        System.out.println("압축전 파일의 크기: "+textSize);
 
+
+        // 압축후 사이즈 체크
+        int sum = 0;
+        for(int i =0;i<arrayList.size();i++){
+            Prefix tempPre = arrayList.get(i);
+            int size = tempPre.key*tempPre.prefix.length();
+            sum += size;
+        }
+        System.out.println("압축후 파일의 크기: "+sum);
 
 
 
